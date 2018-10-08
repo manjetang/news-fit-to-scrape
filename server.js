c0ac14878e3c15bdb2cae6e8150a5621b7aae518
Lionel Manje <manjetang@yahoo.com>
2018-10-07 17:37:08 -0700

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var request = require("request");
 
 // Mongoose
 
-//var Note = require("./models/Note");
+var Note = require("./models/Note");
 var Article = require("./models/Article");
 var db = require("./models");
 
@@ -19,7 +19,8 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<lion>:<Lion1234>@ds225253.mlab.com:25253/mongoscraper";
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://<lion>:<Lion1234>@ds225253.mlab.com:25253/mongoscraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
