974602650285056ef6c53e07314a8200a191895a
Lionel Manje <manjetang@yahoo.com>
2018-10-08 12:27:52 -0700

    changes

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ var request = require("request");
 
 // Mongoose
 
-var Note = require("./models/Note");
-var Article = require("./models/Article");
+//var Note = require("./models/Note");
+//var Article = require("./models/Article");
 var db = require("./models");
 
 var app = express();
